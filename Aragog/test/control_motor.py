e1062a4d954f6a3715f8ac006885647b4eb4d56d
Devin <devin.nielow@web.de>
2025-04-28 02:19:15 +0200

    fixed Controller slowing bot down

---
+++ Aragog/test/control_motor.py
@@ -0,0 +1,32 @@
+import serial
+import serial.tools.list_ports
+
+
+def calculate_checksum(data):
+    checksum = sum(data) & 0xFF
+    return (~checksum) & 0xFF
+
+
+def calc_low_byte(data):
+    return data & 0xFF
+
+
+def calc_high_byte(data):
+    return (data >> 8) & 0xFF
+
+def move(serial, motor, angle):
+    steps = angle*2048
+    packet = [0xFF, 0xFF, motor, 0x07, 0x03, 0x2A, calc_low_byte(steps[0]), calc_high_byte(steps[0])]
+    packet.append(calculate_checksum(packet))
+    serial.write(bytearray(packet))
+
+ports = serial.tools.list_ports.comports()
+
+for port in ports:
+    print(f"Name: {port.device} | Beschreibung: {port.description}")
+
+port = input("Gib einen Port an: ")
+motor = int("Gib einen Motor an: ")
+angle = float(input("Gib einen Winkel an: "))
+serial = serial.Serial(port, 1000000, timeout=1)
+move(serial, motor, angle)
